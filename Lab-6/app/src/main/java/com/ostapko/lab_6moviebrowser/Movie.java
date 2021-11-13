3b976e6b33b34de13b78a572760a37d2a5ee3aa3
210709E01 <210709E01@posta.mu.edu.tr>
2021-11-13 23:25:00 +0300

    Finish Lab 6 Movie Browser

--- Lab-6/app/src/main/java/com/ostapko/lab_6moviebrowser/Movie.java
+++ Lab-6/app/src/main/java/com/ostapko/lab_6moviebrowser/Movie.java
@@ -1,9 +1,12 @@
 package com.ostapko.lab_6moviebrowser;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.ArrayList;
 import java.util.List;
 
-public class Movie {
+public class Movie implements Parcelable {
     private String name;
     private String director;
     private int year;
@@ -57,4 +60,47 @@ public class Movie {
     public void setDescription(String description) {
         this.description = description;
     }
+
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.name);
+        dest.writeString(this.director);
+        dest.writeInt(this.year);
+        dest.writeStringList(this.stars);
+        dest.writeString(this.description);
+    }
+
+    public void readFromParcel(Parcel source) {
+        this.name = source.readString();
+        this.director = source.readString();
+        this.year = source.readInt();
+        this.stars = source.createStringArrayList();
+        this.description = source.readString();
+    }
+
+    protected Movie(Parcel in) {
+        this.name = in.readString();
+        this.director = in.readString();
+        this.year = in.readInt();
+        this.stars = in.createStringArrayList();
+        this.description = in.readString();
+    }
+
+    public static final Parcelable.Creator<Movie> CREATOR = new Parcelable.Creator<Movie>() {
+        @Override
+        public Movie createFromParcel(Parcel source) {
+            return new Movie(source);
+        }
+
+        @Override
+        public Movie[] newArray(int size) {
+            return new Movie[size];
+        }
+    };
 }
